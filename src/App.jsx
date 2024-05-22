da2ef870177f4aef5867685b08cc10a4f819a6cc
CarlosHenriqqq <carlinhoshenrique321123@gmail.com>
2024-05-22 17:19:49 -0300

    Update App.jsx
    Retirei os campos de rota do APP.jsx, pois os campos eram necessários apenas no arquivos do routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
-import InicialPage from "./components/InicialPage";
-import CadastroFornecedor from "./components/Cadastros/CadastroFornecedor";
-import CadastroUsuario from "./components/Cadastros/CadastroUsuario";
+import RouteApp from "../routes";
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/Login" element={<Login />} />
-        <Route path="/PaginaInicial" element={<InicialPage />} />
-        <Route path="/CadastroFornecedor" element={<CadastroFornecedor />} />
-        <Route path="/CadastroUsuario" element={<CadastroUsuario />} />
-      </Routes>
-    </BrowserRouter>
+   <RouteApp/>
   );
 }
 
